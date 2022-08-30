b1c03b390f8e8d7b0f4a4c2b4fe9d1a45bf8f5f4
sgnu <sam.nguyen.1223@gmail.com>
2022-08-30 11:01:35 -0400

    Create basic functions for leaderboard

--- HoshiGameplay/index.js
+++ HoshiGameplay/index.js
@@ -5,3 +5,76 @@ socket.onclose = event => {
     socket.send('Client closed!');
 }
 
+const leaderboardContainer = document.getElementById('leaderboard-container');
+
+/**
+ * Beatmap's MD5 checksum to be updated in-game
+ */
+let leaderboardMD5;
+
+socket.onmessage = event => {
+    const data = JSON.parse(event.data);
+    const menuState = data.menu.state;
+
+    if (menuState === 2) {  // Playing in-game
+        if (data.menu.bm.md5 !== leaderboardMD5) {
+            leaderboardMD5 = data.menu.bm.md5;
+            clearLeaderboardContainer();
+            if (data.gameplay.leaderboard.hasLeaderboard) {
+                generateLeaderboard(data.gameplay.leaderboard);
+            }
+        }
+    }
+}
+
+/**
+ * Remove all child nodes from the leaderboard container
+ */
+function clearLeaderboardContainer() {
+    const leaderboardChildren = leaderboardContainer.children;
+    for (let i = 0; i < leaderboardChildren.length; i++) {
+        leaderboardChildren[i].remove();
+    }
+}
+
+function generateLeaderboard(leaderboard) {
+    const ourPlayer = leaderboard.ourplayer;
+    leaderboard.slots.forEach((slot, index) => {
+        const element = document.createElement('div');
+        if (isOurPlayer(ourPlayer, slot)) {
+            element.id = 'lb-our-player';
+        } else {
+            element.id = `lb-slot-${index}`;
+        }
+        leaderboardContainer.appendChild(element);
+    });
+}
+
+/**
+ * Check if a leaderboard slot is our player
+ * @param {LeaderboardSlot} ourPlayer 
+ * @param {LeaderboardSlot} player 
+ * @returns Boolean
+ */
+function isOurPlayer(ourPlayer, player) {
+    if (ourPlayer.name === player.name) {
+        return (ourPlayer.score === player.score);
+    } else {
+        return false;
+    }
+}
+
+class LeaderboardSlot {
+    name;
+    score;
+    combo;
+    maxCombo;
+    mods;
+    h300;
+    h100;
+    h50;
+    h0;
+    team;
+    position;
+    isPassing;
+}
